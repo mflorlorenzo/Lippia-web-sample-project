1e92cb3767e96eea13ffdd320e0334e19b5c71ba
mflorlorenzo <lorenzo.mflorencia@gmail.com>
2020-04-03 18:23:36 -0300

    adding pase base for phptravels project

---
+++ src/main/java/com/crowdar/examples/pages/PageBasePhpTravels.java
@@ -0,0 +1,13 @@
+package com.crowdar.examples.pages;
+
+import com.crowdar.core.pageObjects.PageBaseWeb;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class PageBasePhpTravels extends PageBaseWeb {
+
+    public PageBasePhpTravels(RemoteWebDriver driver) {
+        super(driver);
+        BASE_URL = "http://www.phptravels.com";
+    }
+
+}
